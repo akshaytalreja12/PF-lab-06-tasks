be1d43276787193227fee69ecaa8a522c6887b74
Akshay Talreja <akshaytalreja512@gmail.com>
2025-10-03 09:03:51 -0700

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+char yes;
+float a , b , c,d; 
 int main(){
 
-float a , b , c,d; 
 printf("enter the number : ");
 scanf("%f",&a);
 printf("enter the length of table from 1 to ");
@@ -10,5 +11,10 @@ for(b=1;b<=c;b++){
 	d =a*b;
 	printf(" %.1f * %.1f = %.1f\n",a,b,d); 
 }
+printf("\nWant to print another table (Y/N)\n");
+scanf(" %c",&yes);
+if (yes=='y' || yes =='Y')
+main();
+
 return 0;
 }
